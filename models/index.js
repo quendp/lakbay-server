642f289bf386a37b8f5d2578a94713a0c830c414
Quen DP <delpilarquen@gmail.com>
2023-03-17 15:53:39 +0800

    Fix env variables bug

--- models/index.js
+++ models/index.js
@@ -9,29 +9,19 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      dialect: "mysql",
-      port: process.env.PORT,
-      host: process.env.DB_HOST,
-    }
-  );
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+let sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    dialect: "mysql",
+    port: process.env.PORT,
+    host: process.env.DB_HOST,
+  }
+);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
